22d6711bcd5ee8e93476d6623b8f85f71bc727d9
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-10-14 00:09:57 +0200

    included version detail

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 import re
 from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 # Get version without importing, which avoids dependency issues
 
 
@@ -13,13 +20,16 @@ def get_version():
 setup(
     name='docker-volume-manager',
     description="Convenient cli app for docker volume management.",
+    long_description=long_description,
     version=get_version(),
     py_modules=['app'],
+    license='MIT',
     include_package_data=True,
     install_requires=['click', 'future'],
     setup_requires=['pytest-runner', 'pytest'],
     tests_require=['pytest', 'pytest-cov', 'coverage'],
     packages=find_packages(),
+    author="Ankur Srivastava",
     download_url="https://github.com/ansrivas/docker-volume-manager/archive/1.0.0.tar.gz",
     entry_points='''
         [console_scripts]
